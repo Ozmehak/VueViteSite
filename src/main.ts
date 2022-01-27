6f183992126986d4a599f00a5882e49d35759eeb
Gibbem <ozmehak.calle@gmail.com>
2022-01-28 00:08:34 +0100

    massa stuff, se notes

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-
+import { registerSW } from 'virtual:pwa-register'
+registerSW()
 createApp(App).use(store).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [vue()]
-
+    plugins: [
+        VitePWA({
+            manifest: {
+                background_color: 'grey',
+                display: 'standalone',
+                icons: [
+                    {
+                        purpose: 'any',
+                        sizes: '192x192',
+                        src: 'manifest-icon-192.maskable.png',
+                        type: 'image/png'
+                    },
+                    {
+                        purpose: 'maskable',
+                        sizes: '192x192',
+                        src: 'manifest-icon-192.maskable.png',
+                        type: 'image/png'
+                    },
+                    {
+                        purpose: 'any',
+                        sizes: '512x512',
+                        src: 'manifest-icon-512.maskable.png',
+                        type: 'image/png'
+                    },
+                    {
+                        purpose: 'maskable',
+                        sizes: '512x512',
+                        src: 'manifest-icon-512.maskable.png',
+                        type: 'image/png'
+                    }
+                ],
+                name: 'DogsNstuff',
+                short_name: 'Doggo',
+                start_url: '.',
+                theme_color: 'red'
+            },
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['assets/**', 'index.html', 'manifest.webmanifest']
+            }
+        }),
+        vue()
+    ]
 })
-;
